d58c5e8a745920e185fe0eb9652695a780014dae
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-04-11 10:49:31 +0100

    Major work done, remains responsiveness

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,3 +24,62 @@ window.addEventListener('scroll', () => {
 
   header.classList.toggle('scroll-header', window.scrollY >= 50)
 })
+
+// Swiper
+
+let swiperPopular = new Swiper('.popular__container', {
+  loop: true,
+  spaceBetween: 25,
+  slidesPerView: 1.4,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      spaceBetween: 48,
+    },
+  },
+})
+
+// Tabs
+let mixerFeatured = mixitup('.featured__content', {
+  selectors: {
+    target: '.featured__card',
+  },
+  animation: {
+    duration: 300,
+  },
+})
+
+// Active featured Link
+
+const linkFeatured = document.querySelectorAll('.featured__item')
+
+function activeLink() {
+  linkFeatured.forEach((g) => {
+    g.classList.remove('active-featured')
+  })
+  this.classList.add('active-featured')
+}
+linkFeatured.forEach((l) => {
+  l.addEventListener('click', activeLink)
+})
+
+// Show scrollUp
+
+function scrollUp() {
+  const scrollUp = document.querySelector('#scroll-up')
+
+  if (this.scrollY >= 350) {
+    scrollUp.classList.add('show-scroll')
+  } else {
+    scrollUp.classList.remove('show-scroll')
+  }
+}
+
+window.addEventListener('scroll', scrollUp)
